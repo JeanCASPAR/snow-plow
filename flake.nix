35e1efdee0867d92fb7f1c7739c282c7bc16ed4d
JeanCASPAR <55629512+JeanCASPAR@users.noreply.github.com>
2024-08-06 16:02:08 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Allows to update all tracked nix flake of your machine in one go";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    utils.url = github:numtide/flake-utils;
+    rust-overlay = {
+      url = github:oxalica/rust-overlay;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk.url = github:nix-community/naersk;
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "clippy" ];
+        };
+        naerskLib = naersk.lib.${system}.override {
+          cargo = rust;
+          rustc = rust;
+        };
+      in {
+        defaultPackage = naerskLib.buildPackage {
+          pname = "nix-update-env";
+          root = ./.;
+        };
+        defaultApp = utils.lib.mkApp {
+          drv = self.defaultPackage.${system};
+        };
+        devShell = with pkgs; mkShell {
+          packages = [
+            rust
+            cargo
+            cargo-edit
+            rustfmt
+            rustPackages.clippy
+            rust-analyzer
+          ];
+        };
+      });
+}
